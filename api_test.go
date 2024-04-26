7f21272d499f9706b52e0e0e345903f6f2c24063
seiya <20365512+seiyab@users.noreply.github.com>
2024-04-27 06:32:42 +0900

    add tests

--- api_test.go
+++ api_test.go
@@ -13,22 +13,35 @@ type test struct {
 	expected []string
 }
 
+type group struct {
+	name  string
+	tests []test
+}
+
 func TestEqual(t *testing.T) {
 	assert := teq.Teq{}
 
-	tests := primitives()
-	for _, test := range tests {
-		name := fmt.Sprintf("%T(%v) == %T(%v)", test.a, test.a, test.b, test.b)
-		t.Run(name, func(t *testing.T) {
-			mt := &mockT{}
-			assert.Equal(mt, test.a, test.b)
-			if len(mt.errors) != len(test.expected) {
-				t.Fatalf("expected %d errors, got %d", len(test.expected), len(mt.errors))
-			}
-			for i, e := range test.expected {
-				if mt.errors[i] != e {
-					t.Errorf("expected %v, got %v at i = %d", e, mt.errors[i], i)
-				}
+	groups := []group{
+		{"primitives", primitives()},
+		{"structs", structs()},
+	}
+
+	for _, group := range groups {
+		t.Run(group.name, func(t *testing.T) {
+			for _, test := range group.tests {
+				name := fmt.Sprintf("%T(%v) == %T(%v)", test.a, test.a, test.b, test.b)
+				t.Run(name, func(t *testing.T) {
+					mt := &mockT{}
+					assert.Equal(mt, test.a, test.b)
+					if len(mt.errors) != len(test.expected) {
+						t.Fatalf("expected %d errors, got %d", len(test.expected), len(mt.errors))
+					}
+					for i, e := range test.expected {
+						if mt.errors[i] != e {
+							t.Errorf("expected %q, got %q at i = %d", e, mt.errors[i], i)
+						}
+					}
+				})
 			}
 		})
 	}
@@ -44,5 +57,19 @@ func primitives() []test {
 		{"a", "b", []string{"expected a, got b"}},
 
 		{"a", 1, []string{"expected a, got 1"}},
+	}
+}
+
+func structs() []test {
+	type s struct {
+		i int
+	}
+	type anotherS struct {
+		i int
+	}
+	return []test{
+		{s{1}, s{1}, nil},
+		{s{1}, s{2}, []string{"expected {1}, got {2}"}},
+		{s{1}, anotherS{1}, []string{"expected {1}, got {1}"}},
 	}
 }
