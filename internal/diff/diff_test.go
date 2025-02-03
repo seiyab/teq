53daa25edd30a4974bed10ec857b10beda8fcc7d
seiya <20365512+seiyab@users.noreply.github.com>
2025-02-03 22:36:53 +0000

    add test

--- internal/diff/diff_test.go
+++ internal/diff/diff_test.go
@@ -124,4 +124,76 @@ func TestDiff_Struct(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("nested struct", func(t *testing.T) {
+		type S1 struct {
+			A int
+			B string
+		}
+		type S2 struct {
+			X S1
+			Y S1
+		}
+		type testCase struct {
+			name  string
+			left  S2
+			right S2
+			want  string
+		}
+		for _, tc := range []testCase{
+			{
+				name:  "completely different",
+				left:  S2{X: S1{A: 1, B: "hello"}, Y: S1{A: 2, B: "world"}},
+				right: S2{X: S1{A: 2, B: "world"}, Y: S1{A: 1, B: "hello"}},
+				want: strings.Join([]string{
+					`  diff_test.S2{`,
+					`    X: diff_test.S1{`,
+					`-     A: 1,`,
+					`+     A: 2,`,
+					`-     B: "hello",`,
+					`+     B: "world",`,
+					`    },`,
+					`    Y: diff_test.S1{`,
+					`-     A: 2,`,
+					`+     A: 1,`,
+					`-     B: "world",`,
+					`+     B: "hello",`,
+					`    },`,
+					`  }`,
+				}, "\n"),
+			},
+			{
+				name:  "partially different",
+				left:  S2{X: S1{A: 1, B: "hello"}, Y: S1{A: 1, B: "world"}},
+				right: S2{X: S1{A: 1, B: "world"}, Y: S1{A: 1, B: "hello"}},
+				want: strings.Join([]string{
+					`  diff_test.S2{`,
+					`    X: diff_test.S1{`,
+					`      A: 1,`,
+					`-     B: "hello",`,
+					`+     B: "world",`,
+					`    },`,
+					`    Y: diff_test.S1{`,
+					`      A: 1,`,
+					`-     B: "world",`,
+					`+     B: "hello",`,
+					`    },`,
+					`  }`,
+				}, "\n"),
+			},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				d, e := diff.New().Diff(tc.left, tc.right)
+				if e != nil {
+					t.Fatal(e)
+				}
+				f := d.Format()
+				if f != tc.want {
+					t.Errorf("expected %q, got %q", tc.want, f)
+					t.Log(f)
+					t.Log(tc.want)
+				}
+			})
+		}
+	})
 }
