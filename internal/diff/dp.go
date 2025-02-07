214a8b1af18e95a8ad50ae1fa60fceeb876033f0
seiya <20365512+seiyab@users.noreply.github.com>
2025-02-07 23:19:23 +0000

    refactor

--- internal/diff/dp.go
+++ internal/diff/dp.go
@@ -14,62 +14,65 @@ type dpCell struct {
 	fromB int
 }
 
-func sliceMixedEntries(v1, v2 reflect.Value, nx next) ([]entry, error) {
-	if v1.Kind() != reflect.Slice || v2.Kind() != reflect.Slice {
-		return nil, errors.New("unexpected kind")
-	}
+func mixedEntries[Value any, List any](
+	v1, v2 List,
+	length func(List) int,
+	getValue func(List, int) Value,
+	getReflect func(List, int) reflect.Value,
+	nx func(Value, Value) (DiffTree, error),
+) ([]entry, error) {
 	leading := make([]entry, 0)
-	for i := 0; i < v1.Len() && i < v2.Len(); i++ {
-		t, err := nx(v1.Index(i), v2.Index(i))
+	for i := 0; i < length(v1) && i < length(v2); i++ {
+		t, err := nx(getValue(v1, i), getValue(v2, i))
 		if err != nil {
 			return nil, err
 		}
 		if t.loss > 0 {
 			break
 		}
-		leading = append(leading, entry{value: same(v1.Index(i))})
+		leading = append(leading, entry{value: same(getReflect(v1, i))})
 	}
 	k := len(leading)
 
-	dp := make([][]dpCell, v1.Len()-k+1)
+	dp := make([][]dpCell, length(v1)-k+1)
 	for i := range dp {
-		dp[i] = make([]dpCell, v2.Len()-k+1)
+		dp[i] = make([]dpCell, length(v2)-k+1)
 		for j := range dp[i] {
 			dp[i][j] = dpCell{loss: math.MaxFloat64}
 		}
 	}
 	dp[0][0] = dpCell{loss: 0}
-	for a := 0; k+a < v1.Len()+1; a++ {
-		for b := 0; k+b < v2.Len()+1; b++ {
+	for a := 0; k+a < length(v1)+1; a++ {
+		for b := 0; k+b < length(v2)+1; b++ {
 			l := dp[a][b].loss
-			if k+a < v1.Len() {
+			if k+a < length(v1) {
 				if l+1 < dp[a+1][b].loss {
 					dp[a+1][b] = dpCell{
 						loss: l + 1,
 						entry: entry{
 							leftOnly: true,
-							value:    imbalanced(v1.Index(k + a)),
+							value:    imbalanced(getReflect(v1, k+a)),
 						},
 						fromA: a,
 						fromB: b,
 					}
 				}
 			}
-			if k+b < v2.Len() {
+			if k+b < length(v2) {
 				if l+1 < dp[a][b+1].loss {
 					dp[a][b+1] = dpCell{
 						loss: l + 1,
 						entry: entry{
 							rightOnly: true,
-							value:     imbalanced(v2.Index(k + b)),
+							value:     imbalanced(getReflect(v2, k+b)),
 						},
 						fromA: a,
 						fromB: b,
 					}
 				}
 			}
-			if k+a < v1.Len() && k+b < v2.Len() {
-				t, err := nx(v1.Index(k+a), v2.Index(k+b))
+			if k+a < length(v1) && k+b < length(v2) {
+				t, err := nx(getValue(v1, k+a), getValue(v2, k+b))
 				if err != nil {
 					return nil, err
 				}
@@ -91,11 +94,11 @@ func sliceMixedEntries(v1, v2 reflect.Value, nx next) ([]entry, error) {
 	a := len(dp) - 1
 	b := len(dp[a]) - 1
 	if dp[a][b].loss > 1_000_000 {
-		return nil, fmt.Errorf("faild to compute diff")
+		return nil, fmt.Errorf("failed to compute diff")
 	}
 
-	trailing := make([]entry, 0, v1.Len()+v2.Len())
-	for a, b := v1.Len()-k, v2.Len()-k; a > 0 || b > 0; {
+	trailing := make([]entry, 0, length(v1)+length(v2))
+	for a, b := length(v1)-k, length(v2)-k; a > 0 || b > 0; {
 		cell := dp[a][b]
 		trailing = append(trailing, cell.entry)
 		if !(cell.fromA < a || cell.fromB < b) {
@@ -111,89 +114,33 @@ func sliceMixedEntries(v1, v2 reflect.Value, nx next) ([]entry, error) {
 	return entries, nil
 }
 
+func sliceMixedEntries(v1, v2 reflect.Value, nx next) ([]entry, error) {
+	if v1.Kind() != reflect.Slice || v2.Kind() != reflect.Slice {
+		return nil, errors.New("unexpected kind")
+	}
+	es, err := mixedEntries(
+		v1, v2,
+		func(v reflect.Value) int { return v.Len() },
+		func(v reflect.Value, i int) reflect.Value { return v.Index(i) },
+		func(v reflect.Value, i int) reflect.Value { return v.Index(i) },
+		nx,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return es, nil
+}
+
 func multiLineStringEntries(v1, v2 []string) ([]entry, error) {
-	leading := make([]entry, 0)
-	for i := 0; i < len(v1) && i < len(v2); i++ {
-		if v1[i] != v2[i] {
-			break
-		}
-		leading = append(leading, entry{value: same(reflect.ValueOf(v1[i]))})
-	}
-	k := len(leading)
-
-	dp := make([][]dpCell, len(v1)-k+1)
-	for i := range dp {
-		dp[i] = make([]dpCell, len(v2)-k+1)
-		for j := range dp[i] {
-			dp[i][j] = dpCell{loss: math.MaxFloat64}
-		}
-	}
-	dp[0][0] = dpCell{loss: 0}
-
-	for a := 0; k+a < len(v1)+1; a++ {
-		for b := 0; k+b < len(v2)+1; b++ {
-			l := dp[a][b].loss
-			if k+a < len(v1) {
-				if l+1 < dp[a+1][b].loss {
-					dp[a+1][b] = dpCell{
-						loss: l + 1,
-						entry: entry{
-							leftOnly: true,
-							value:    imbalanced(reflect.ValueOf(v1[k+a])),
-						},
-						fromA: a,
-						fromB: b,
-					}
-				}
-			}
-			if k+b < len(v2) {
-				if l+1 < dp[a][b+1].loss {
-					dp[a][b+1] = dpCell{
-						loss: l + 1,
-						entry: entry{
-							rightOnly: true,
-							value:     imbalanced(reflect.ValueOf(v2[k+b])),
-						},
-						fromA: a,
-						fromB: b,
-					}
-				}
-			}
-			if k+a < len(v1) && k+b < len(v2) {
-				if v1[k+a] == v2[k+b] {
-					if l < dp[a+1][b+1].loss {
-						dp[a+1][b+1] = dpCell{
-							loss:  l,
-							entry: entry{value: same(reflect.ValueOf(v1[k+a]))},
-							fromA: a,
-							fromB: b,
-						}
-					}
-				}
-			}
-		}
-	}
-	a := len(dp) - 1
-	b := len(dp[a]) - 1
-	if dp[a][b].loss > 1_000_000 {
-		return nil, fmt.Errorf("faild to compute diff")
-	}
-
-	trailing := make([]entry, 0, len(v1)+len(v2))
-	for a, b := len(v1)-k, len(v2)-k; a > 0 || b > 0; {
-		cell := dp[a][b]
-		trailing = append(trailing, cell.entry)
-		if !(cell.fromA < a || cell.fromB < b) {
-			return nil, fmt.Errorf("infinite loop")
-		}
-		a = cell.fromA
-		b = cell.fromB
-	}
-	reverse(trailing)
-
-	entries := append(leading, trailing...)
-
-	return entries, nil
+	return mixedEntries(
+		v1, v2,
+		func(v []string) int { return len(v) },
+		func(v []string, i int) string { return v[i] },
+		func(v []string, i int) reflect.Value { return reflect.ValueOf(v[i]) },
+		func(v1, v2 string) (DiffTree, error) {
+			return stringDiff(reflect.ValueOf(v1), reflect.ValueOf(v2), nil)
+		},
+	)
 }
 
 func reverse(entries []entry) {
